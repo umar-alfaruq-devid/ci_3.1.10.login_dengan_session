c0acdf4c7c8412d3efa67a6ec2bb6bb368ed1709
umar.alfaruq.devid@gmail.com <umar.alfaruq.devid@gmail.com>
2019-10-06 07:38:41 +0700

    block access

--- application/controllers/signupController.php
+++ application/controllers/signupController.php
@@ -4,17 +4,31 @@
 	 */
 	class signupController extends CI_Controller
 	{
+
+		public $statusLogin = false;
 		
 		public function __construct()
 		{
 			parent::__construct();
 			$this->load->library('form_validation');
 			$this->load->model('account/insertUser');
+			if($this->session->userdata('username') != false) {
+				$this->statusLogin = true;
+			}
 		}
 
 		public function index() 
 		{
-			$this->load->view('auth/signup');
+			if($this->statusLogin != true) {
+				$this->load->view('auth/signup');
+			} else {
+				$informasi = array(
+					'message' => 'Are you loged in',
+					'buttonText' => 'Go home',
+					'getLink' => 'home'
+				);
+				$this->load->view('alert/err', $informasi);
+			}
 		}
 
 		/**
@@ -22,51 +36,60 @@
 		 */
 		public function signupAct()
 		{
-			$user = $this->input->post('user');
-			$pass = $this->input->post('pass');
+			if($this->statusLogin != true) {
+				$user = $this->input->post('user');
+				$pass = $this->input->post('pass');
 
-			/**
-			 * set pesan
-			 */
+				/**
+				 * set pesan
+				 */
 
-			$this->form_validation->set_message('required', '{field} tidak boleh kosong');
-			$this->form_validation->set_message('alpha_numeric', '{field} hanya angka dan huruf');
-			$this->form_validation->set_message('min_length', '{field} minimal {param} karakter');
-			$this->form_validation->set_message('is_unique', '{field} sudah di gunakan');
-			$this->form_validation->set_message('matches', '{field} tidak sama dengan {param}');
+				$this->form_validation->set_message('required', '{field} tidak boleh kosong');
+				$this->form_validation->set_message('alpha_numeric', '{field} hanya angka dan huruf');
+				$this->form_validation->set_message('min_length', '{field} minimal {param} karakter');
+				$this->form_validation->set_message('is_unique', '{field} sudah di gunakan');
+				$this->form_validation->set_message('matches', '{field} tidak sama dengan {param}');
 
-			 /**
-			 * set peraturan validasi
-			 */
-			$this->form_validation->set_rules('user', 'Username', 'required|alpha_numeric|min_length[8]|is_unique[accounts.username]');
-			$this->form_validation->set_rules('pass', 'Password', 'required|alpha_numeric|min_length[8]');
-			$this->form_validation->set_rules('retype', 'Ulang password', 'required|alpha_numeric|matches[pass]');
-			if($this->form_validation->run() == false) {
-				$this->load->view('auth/signup');
-			} else {
-				$data = array(
-					'username' => $user,
-					'password' => $pass,
-					'created_at' => date('Y-m-d H:i:s')
-				);
-				$queryIsi = $this->insertUser->inserAccount('accounts', $data);
-				if($queryIsi) {
-					$informasi = array(
-						'message' => 'Account is created',
-						'buttonText' => 'Go home',
-						'getLink' => 'home'
-					);
-
-					$this->load->view('alert/success', $informasi);
+				 /**
+				 * set peraturan validasi
+				 */
+				$this->form_validation->set_rules('user', 'Username', 'required|alpha_numeric|min_length[8]|is_unique[accounts.username]');
+				$this->form_validation->set_rules('pass', 'Password', 'required|alpha_numeric|min_length[8]');
+				$this->form_validation->set_rules('retype', 'Ulang password', 'required|alpha_numeric|matches[pass]');
+				if($this->form_validation->run() == false) {
+					$this->load->view('auth/signup');
 				} else {
-					$informasi = array(
-						'message' => 'Some thing whent wrong',
-						'buttonText' => 'Signup',
-						'getLink' => 'signup'
+					$data = array(
+						'username' => $user,
+						'password' => $pass,
+						'created_at' => date('Y-m-d H:i:s')
 					);
+					$queryIsi = $this->insertUser->inserAccount('accounts', $data);
+					if($queryIsi) {
+						$informasi = array(
+							'message' => 'Account is created',
+							'buttonText' => 'Go home',
+							'getLink' => 'home'
+						);
 
-					$this->load->view('alert/err', $informasi);
+						$this->load->view('alert/success', $informasi);
+					} else {
+						$informasi = array(
+							'message' => 'Some thing whent wrong',
+							'buttonText' => 'Signup',
+							'getLink' => 'signup'
+						);
+
+						$this->load->view('alert/err', $informasi);
+					}
 				}
+			} else {
+				$informasi = array(
+					'message' => 'You\'re loged in',
+					'buttonText' => 'Logout',
+					'getLink' => 'logout'
+				);
+				$this->load->view('alert/err', $informasi);
 			}
 		}
 
